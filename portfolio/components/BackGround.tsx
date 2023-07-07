ef370fc70a43251dd325a022264fa54555eef9bc
Marvin <marvinnrn@gmail.com>
2023-07-07 16:46:07 +0200

    reboot du projet portfolio en typescript

---
+++ portfolio/components/BackGround.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+
+interface CustomCSSProperties extends React.CSSProperties {
+    '--i'?: number;
+}
+
+const BackGround: React.FC = () => {
+    return (
+        <div className='bg'>
+            <div className="bg__bubbles">
+                <span style={{ '--i': 11 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 12 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 24 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 10 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 14 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 23 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 18 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 16 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 19 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 20 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 22 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 25 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 18 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 21 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 15 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 13 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 26 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 17 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 13 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 28 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 11 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 12 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 24 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 10 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 14 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 23 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 18 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 16 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 19 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 20 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 22 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 25 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 18 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 21 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 15 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 13 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 26 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 17 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 13 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 28 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 11 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 12 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 24 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 10 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 14 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 23 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 18 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 16 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 19 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 20 } as CustomCSSProperties}></span>
+                <span style={{ '--i': 22 } as CustomCSSProperties}></span>
+            </div>
+        </div>
+    );
+};
+
+export default BackGround;
---
+++ portfolio/pages/IntroPart.tsx
@@ -0,0 +1,19 @@
+import React, { MouseEvent } from 'react';
+import BackGround from '../components/BackGround';
+
+const IntroPart = () => {
+    return (
+        <section className='intro'>
+            <BackGround />
+            <div className='intro__content'>
+                <h1 className='intro__title'>Marvin Rodrigues Novo</h1>
+                <p className='intro__subtitle'>Développeur Web Front-End</p>
+                <button className='intro__btn'>
+                    Qui suis-je?
+                </button>
+            </div>
+        </section>
+    );
+};
+
+export default IntroPart;
