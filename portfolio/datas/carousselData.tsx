61d2bab70c996e0e6c364bd5bb9288d98c69f4eb
Marvin <marvinnrn@gmail.com>
2023-07-17 14:41:11 +0200

    creation des caroussel dans la partie myProjects + animation hover des slides

---
+++ portfolio/datas/carousselData.tsx
@@ -0,0 +1,91 @@
+import groupomaniaV2Registration from '../assets/myProjects/RegistrationGroupomania.png'
+import groupomaniaV2Login from '../assets/myProjects/LoginGroupomania.png'
+import groupomaniaV2Home from '../assets/myProjects/HomeGroupomania.png'
+import groupomaniaV2Modal from '../assets/myProjects/ModalGroupomania.png'
+
+import pickerWheelHome from '../assets/myProjects/PickerwheelHome.png'
+import pileOuFaceHome from '../assets/myProjects/PileouFaceHome.png'
+import pickerWheel from '../assets/myProjects/PickerWheel.png'
+import pickerWheelwin from '../assets/myProjects/PickerwheelWin.png'
+import pileOuFace from '../assets/myProjects/PileouFace.png'
+
+import odenWeebHome from '../assets/myProjects/OdenWeebHome.png'
+import odenWeebTops from '../assets/myProjects/OdenWeebTops.png'
+import odenWeebTopOpen from '../assets/myProjects/OdenWeebTopOpen.png'
+import odenWeebReco from '../assets/myProjects/OdenWeebRecomandation.png'
+import odenWeebCaroussel from '../assets/myProjects/OdenWeebCaroussel.png'
+
+export const GroupomaniaV2 = [
+    {
+        id: 1,
+        cover: groupomaniaV2Registration,
+    },
+
+    {
+        id: 2,
+        cover: groupomaniaV2Login
+    },
+
+    {
+        id: 3,
+        cover: groupomaniaV2Home
+    },
+
+    {
+        id: 4,
+        cover: groupomaniaV2Modal
+    }
+]
+
+export const PickerWheel = [
+    {
+        id: 1,
+        cover: pickerWheelHome,
+    },
+
+    {
+        id: 2,
+        cover: pileOuFaceHome
+    },
+
+    {
+        id: 3,
+        cover: pickerWheel
+    },
+    {
+        id: 4,
+        cover: pickerWheelwin
+    },
+
+    {
+        id: 5,
+        cover: pileOuFace
+    }
+]
+
+
+export const OdenWeeb = [
+    {
+        id: 1,
+        cover: odenWeebHome,
+    },
+
+    {
+        id: 2,
+        cover: odenWeebTops
+    },
+
+    {
+        id: 3,
+        cover: odenWeebTopOpen
+    },
+    {
+        id: 4,
+        cover: odenWeebReco
+    },
+
+    {
+        id: 5,
+        cover: odenWeebCaroussel
+    }
+]
--- portfolio/pages/MyProjects.tsx
+++ portfolio/pages/MyProjects.tsx
@@ -1,25 +1,63 @@
 import React from 'react';
 import BackGround3 from '../components/BackGround3';
+import Image from 'next/image';
+import { GroupomaniaV2 } from '@/datas/carousselData';
+import { PickerWheel } from '@/datas/carousselData';
+import { OdenWeeb } from '@/datas/carousselData';
+import Slider from 'react-slick'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 
 const MyProjects = () => {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    };
+
     return (
         <section id='myProjects' className='myProjects'>
             <div className='myProjects__content'>
                 <h2 className='myProjects__title'>Mes Projets</h2>
                 <div className='myProjects__cards--flex'>
                     <div className='myProjects__cards'>
-                        <h3>Projet 1</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod commodo eros, sed fringilla purus tristique in. Sed bibendum, diam sed finibus feugiat, justo lectus vestibulum nulla, nec tincidunt lacus lorem id diam. Mauris malesuada justo in ex tempus, vitae varius tortor tristique. In hac habitasse platea dictumst. Nulla nec leo rutrum, sollicitudin nunc id, laoreet purus. Donec semper, mauris vel dignissim fringilla, lorem urna blandit dui, eget posuere sapien neque vitae nunc. Proin rutrum augue a ex tincidunt, eu ullamcorper nulla dapibus. Maecenas eu erat vitae tellus condimentum scelerisque</p>
-                        <button>Test</button>
+                        <h3>GroupomaniaV2</h3>
+                        <Slider {...settings} className='caroussel'>
+                            {GroupomaniaV2.map(({ id, cover }) => {
+                                return (
+                                    <div key={id}>
+                                        <Image className='myProjects__img' src={cover} alt='test' width={500} height={200} />
+                                    </div>
+                                )
+                            })}
+                        </Slider>
                     </div>
                     <div className='myProjects__cards'>
-                        <h3>Projet 1</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod commodo eros, sed fringilla purus tristique in. Sed bibendum, diam sed finibus feugiat, justo lectus vestibulum nulla, nec tincidunt lacus lorem id diam. Mauris malesuada justo in ex tempus, vitae varius tortor tristique. In hac habitasse platea dictumst. Nulla nec leo rutrum, sollicitudin nunc id, laoreet purus. Donec semper, mauris vel dignissim fringilla, lorem urna blandit dui, eget posuere sapien neque vitae nunc. Proin rutrum augue a ex tincidunt, eu ullamcorper nulla dapibus. Maecenas eu erat vitae tellus condimentum scelerisque</p>
+                        <h3>PickerWheel</h3>
+                        <Slider {...settings} className='caroussel'>
+                            {PickerWheel.map(({ id, cover }) => {
+                                return (
+                                    <div key={id}>
+                                        <Image className='myProjects__img' src={cover} alt='test' width={500} height={200} />
+                                    </div>
+                                )
+                            })}
+                        </Slider>
                     </div>
                     <div className='myProjects__cards'>
-                        <h3>Projet 1</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod commodo eros, sed fringilla purus tristique in. Sed bibendum, diam sed finibus feugiat, justo lectus vestibulum nulla, nec tincidunt lacus lorem id diam. Mauris malesuada justo in ex tempus, vitae varius tortor tristique. In hac habitasse platea dictumst. Nulla nec leo rutrum, sollicitudin nunc id, laoreet purus. Donec semper, mauris vel dignissim fringilla, lorem urna blandit dui, eget posuere sapien neque vitae nunc. Proin rutrum augue a ex tincidunt, eu ullamcorper nulla dapibus. Maecenas eu erat vitae tellus condimentum scelerisque</p>
+                        <h3>OdenWeeb</h3>
+                        <Slider {...settings} className='caroussel'>
+                            {OdenWeeb.map(({ id, cover }) => {
+                                return (
+                                    <div key={id}>
+                                        <Image className='myProjects__img' src={cover} alt='test' width={500} height={200} />
+                                    </div>
+                                )
+                            })}
+                        </Slider>
                     </div>
                 </div>
             </div>
