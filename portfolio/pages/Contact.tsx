5ff93ac3a16226370efa0ceade2900b026bba98c
Marvin <marvinnrn@gmail.com>
2023-07-17 18:42:07 +0200

    partie contact finito + boutton pour telecharger cv

--- portfolio/pages/Contact.tsx
+++ portfolio/pages/Contact.tsx
@@ -6,11 +6,14 @@ const Contact = () => {
         <section id='contact' className='contact'>
             <BackGround4 />
             <div className='contact__content'>
-                <h1 className='contact__title'>Marvin Rodrigues Novo</h1>
-                <p className='contact__subtitle'>Développeur Web Front-End</p>
-                <button className='contact__btn' >Qui suis-je?</button>
+                <h1 className='contact__title'>Contact</h1>
+                <p className='contact__subtitle'>Si vous souhaitez me contacter, vous pouvez simplement m'envoyer un e-mail à l'adresse suivante:</p>
+                <a className='contact__mail' href="mailto:marvinnrn@gmail.com" >marvinnrn@gmail.com</a>
+                <a className='contact__btnLink' href='/CV_RODRIGUESNOVO_Marvin.pdf' download="CV_RODRIGUESNOVO_Marvin">
+                    <button className='contact__btn' >Télecharger mon CV</button>
+                </a>
             </div>
-        </section>
+        </section >
     );
 };
 
